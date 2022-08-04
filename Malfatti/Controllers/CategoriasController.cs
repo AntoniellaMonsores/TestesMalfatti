e8ba897846ed0e4502b9047b60bfd000a8ee24fa
Antoniella Monsores <antoniella.monsores@academico.ifrn.edu.br>
2022-08-04 11:27:08 -0300

    Update

--- Malfatti/Controllers/CategoriasController.cs
+++ Malfatti/Controllers/CategoriasController.cs
@@ -14,15 +14,6 @@ namespace Malfatti.Controllers
     {
         private EFContext context = new EFContext();
 
-        /*private static IList<Categoria> categorias = new List<Categoria>()
-        {
-            new Categoria() { CategoriaId = 1, Nome = "Anéis"},
-            new Categoria() { CategoriaId = 2, Nome = "Braceletes"},
-            new Categoria() { CategoriaId = 3, Nome = "Brincos"},
-            new Categoria() { CategoriaId = 4, Nome = "Colares"},
-            new Categoria() { CategoriaId = 5, Nome = "Relógios"}
-        };*/
-
         public ActionResult Index()
         {
             return View(context.Categorias.OrderBy(c => c.Nome));
