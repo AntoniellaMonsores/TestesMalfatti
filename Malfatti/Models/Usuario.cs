4db57ca6b4354aeb277bb0902e3145369452fb8d
Maria-Ricarte <97289630+Maria-Ricarte@users.noreply.github.com>
2022-08-10 12:50:11 -0300

    Update and rename Cliente.cs to Usuario.cs

--- Malfatti/Models/Usuario.cs
+++ Malfatti/Models/Usuario.cs
@@ -1,17 +1,17 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace Malfatti.Models
 {
-    public class Cliente
+    public class Usuario
     {
-        public long ClienteId { get; set; }
+        public int UsuarioId { get; set; }
+        public string Username { get; set; }
         public string Nome { get; set; }
         public string Email { get; set; }
         public string Telefone { get; set; }
-        public string Login { get; set; }
         public string Senha { get; set; }
         public string[] Endereco { get; set; }
         public long[] ItemSacola { get; set; }
